e429ab0c3f9a78cd205c804e8d84a4181cec3ba0
Loz <nicollaur@gmail.com>
2022-10-04 14:52:26 -0400

    watched dors coding school solution

--- script.js
+++ script.js
@@ -1,53 +1,129 @@
-const display = document.querySelector("#calc-text")
-const allBtns = document.querySelectorAll("button").forEach(btn => {
-    return btn.addEventListener("click", function (e) {
-        let total = [];
-        let operator = "";
+let currentValue = "";
+let prevValue = "";
+let operator = "";
 
-        if (
-            e.target.innerText === "+" ||
-            e.target.innerText === "-" ||
-            e.target.innerText === "x" ||
-            e.target.innerText === "÷"
-        ) {
-            operator = e.target.innerText;
-        } else if (e.target.innerText === "=") {
-            display.textContent = total;
-        } else if (e.target.innerText === "clear") {
-            total = [];
-            display.textContent = "Calc of DOOM";
-        } else {
-            total += e.target.innerText;
+
+document.addEventListener("DOMContentLoaded", function () {
+    //Store all components of HTML
+    let clear = document.querySelector(".all-clear");
+    let equal = document.querySelector(".equals");
+    let decimal = document.querySelector(".decimal");
+    let del = document.querySelector(".delete");
+
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+
+    let prevScreen = document.querySelector(".previous-operand");
+    let currentScreen = document.querySelector(".current-operand");
+
+    numbers.forEach((number) => number.addEventListener("click", function (e) {
+        handleNumber(e.target.textContent);
+        currentScreen.textContent = currentValue;
+    }))
+
+    operators.forEach((op) => op.addEventListener("click", function (e) {
+        handleOperator(e.target.textContent);
+        prevScreen.textContent = prevValue + " " + operator;
+        currentScreen.textContent = currentValue;
+    }))
+
+    clear.addEventListener("click", function () {
+        prevValue = "";
+        currentValue = "";
+        operator = "";
+        prevScreen.textContent = "Calc of";
+        currentScreen.textContent = "DOOM";
+    })
+
+    equal.addEventListener("click", function () {
+        if (currentValue != "" && prevValue != "") {
+            calculate();
+            prevScreen.textContent = "";
+            currentScreen.textContent = prevValue;
         }
-        console.log(total);
+    })
+
+    decimal.addEventListener("click", function () {
+        addDecimal();
+    })
+
+    del.addEventListener("click", function () {
+        currentScreen.textContent = currentScreen.textContent.slice(0, -1);
+        currentValue = currentScreen.textContent;
+        if (currentScreen.textContent === "") {
+            currentValue = "";
+            currentScreen.textContent = "DOOM";
+        }
     })
 });
 
+function handleNumber(num) {
+    currentValue += num;
+}
 
-function operate(operator, num1, num2) {
+function handleOperator(op) {
+    operator = op;
+    prevValue = currentValue;
+    currentValue = "";
+}
+
+function calculate() {
+    prevValue = +(prevValue);
+    currentValue = +(currentValue);
+
     if (operator === "+") {
-        return add(num1, num2);
+        prevValue += currentValue;
     } else if (operator === "-") {
-        return subtract(num1, num2);
+        prevValue -= currentValue;
     } else if (operator === "x") {
-        return multiply(num1, num2);
+        prevValue *= currentValue;
     } else if (operator === "÷") {
-        return divide(num1, num2);
+        prevValue /= currentValue;
     }
-};
 
-function add(...num) {
-    return num.reduce((a, b) => a + b)
-};
+    prevValue = roundNumber(prevValue);
+    prevValue = prevValue.toString();
+    currentValue = prevValue.toString();
+}
 
-function subtract(...num) {
-    return num.reduce((a, b) => a - b)
-};
+function roundNumber(num) {
+    return Math.round(num * 1000) / 1000;
+}
 
-function multiply(...num) {
-    return num.reduce((a, b) => a * b)
-};
+function addDecimal() {
+    if (!currentValue.includes(".")) {
+        currentValue += ".";
+    }
+}
 
-function divide(...num) {
-    return num.reduce((a, b) => a / b)
-};
+
+
+
+
+// function operate(operator, num1, num2) {
+//     if (operator === "+") {
+//         return add(num1, num2);
+//     } else if (operator === "-") {
+//         return subtract(num1, num2);
+//     } else if (operator === "x") {
+//         return multiply(num1, num2);
+//     } else if (operator === "÷") {
+//         return divide(num1, num2);
+//     }
+// };
+
+// function add(...num) {
+//     return num.reduce((a, b) => a + b)
+// };
+
+// function subtract(...num) {
+//     return num.reduce((a, b) => a - b)
+// };
+
+// function multiply(...num) {
+//     return num.reduce((a, b) => a * b)
+// };
+
+// function divide(...num) {
+//     return num.reduce((a, b) => a / b)
+// };
