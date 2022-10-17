9775c6a1a85f9f40b884a3e7ad4946d0dceabb6f
Loz <nicollaur@gmail.com>
2022-10-17 11:09:09 -0400

    add numBtns addEventListener; add displayValue variable; add calcScreenCurrent to display displayValue

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-let total = 0;
+const calcScreenCurrent = document.querySelector(".current-operand");
+const numBtns = document.querySelectorAll(".number");
+
+let displayValue = 0;
+
+numBtns.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+        displayValue = e.target.innerHTML;
+        calcScreenCurrent.textContent = e.target.innerHTML;
+    })
+})
 
 function operate(operator, num1, num2) {
     if (operator === "+") {
