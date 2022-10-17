8404bead5a1f423391a3988d566639b8711f620a
Loz <nicollaur@gmail.com>
2022-10-17 10:49:09 -0400

    add calculator function operate

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 let total = 0;
 
+function operate(operator, num1, num2) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    }
+};
+
 function add(...num) {
     return num.reduce((acc, cur) => {
         return acc + cur;
     })
-}
+};
 
 function subtract(...num) {
     return num.reduce((acc, cur) => {
         return acc - cur;
     })
-}
+};
 
 function multiply(...num) {
     return num.reduce((acc, cur) => {
         return acc * cur;
     })
-}
+};
 
 function divide(...num) {
     return num.reduce((acc, cur) => {
         return acc / cur;
     })
-}
+};
